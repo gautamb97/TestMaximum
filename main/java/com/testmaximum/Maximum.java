0c90351620885edadbbcc5239e2b056aeb3515ff
gautamb97 <www.gautam971997@gmail.com>
2021-03-01 21:04:32 +0530

    Refactored:To generic method and find maximum

--- main/java/com/testmaximum/Maximum.java
+++ main/java/com/testmaximum/Maximum.java
@@ -1,46 +1,20 @@
 package com.testmaximum;
 
 public class Maximum {
-    public static Integer maxInteger(Integer x, Integer y, Integer z){
-        Integer max = x;
+    public static <T extends Comparable<T>> T maximumValue(T x, T y, T z){
+        T max = x;
         if(y.compareTo(max) > 0){
             max = y;
         }
         if(z.compareTo(max) > 0){
             max = z;
         }
-        printMaxInteger(x, y, z, max);
+        printMax(x, y, z, max);
         return max;
     }
-    public static Float maxFloat(Float x, Float y, Float z){
-        Float max = x;
-        if(y.compareTo(max) > 0){
-            max = y;
-        }
-        if(z.compareTo(max) > 0){
-            max = z;
-        }
-        printMaxFloat(x, y, z, max);
-        return max;
-    }
-    public static String maxString(String x, String y, String z){
-        String max = x;
-        if(y.compareTo(max) > 0){
-            max = y;
-        }
-        if(z.compareTo(max) > 0){
-            max = z;
-        }
-        printMaxString(x, y, z, max);
-        return max;
-    }
-    private static void printMaxInteger(Integer x, Integer y, Integer z, Integer max){
+
+    private static <T> void printMax(T x, T y, T z, T max){
         System.out.println("Maximum integer value is : "+max);
     }
-    private static void printMaxFloat(Float x, Float y, Float z, Float max){
-        System.out.println("Maximum integer value is : "+max);
-    }
-    private static void printMaxString(String x, String y, String z, String max){
-        System.out.println("Maximum integer value is : "+max);
-    }
+
 }
--- test/java/com/testmaximum/MaximumTest.java
+++ test/java/com/testmaximum/MaximumTest.java
@@ -7,47 +7,47 @@ public class MaximumTest {
     Maximum maximum = new Maximum();
     @Test
     public void givenInteger_WhenFirstInteger_IsMaxShouldReturnMax(){
-        int maximumInteger = maximum.maxInteger(9,5,2);
+        int maximumInteger = maximum.maximumValue(9,5,2);
         Assert.assertEquals(9, maximumInteger);
     }
     @Test
     public void givenInteger_WhenSecondInteger_IsMaxShouldReturnMax(){
-        int maximumInteger = maximum.maxInteger(5,9,2);
+        int maximumInteger = maximum.maximumValue(5,9,2);
         Assert.assertEquals(9, maximumInteger);
     }
     @Test
     public void givenInteger_WhenThirdInteger_IsMaxShouldReturnMax(){
-        int maximumInteger = maximum.maxInteger(5,2,9);
+        int maximumInteger = maximum.maximumValue(5,2,9);
         Assert.assertEquals(9, maximumInteger);
     }
     @Test
     public void givenFloat_WhenFirstFloat_IsMaxShouldReturnMax(){
-        float maximumFloat = maximum.maxFloat(9.1f,5.1f,2.1f);
+        float maximumFloat = maximum.maximumValue(9.1f,5.1f,2.1f);
         Assert.assertEquals(9.1f, maximumFloat,0.0f);
     }
     @Test
     public void givenFloat_WhenSecondFloat_IsMaxShouldReturnMax(){
-        float maximumFloat = maximum.maxFloat(5.1f,9.1f,2.1f);
+        float maximumFloat = maximum.maximumValue(5.1f,9.1f,2.1f);
         Assert.assertEquals(9.1f, maximumFloat,0.0f);
     }
     @Test
     public void givenFloat_WhenThirdFloat_IsMaxShouldReturnMax(){
-        float maximumFloat = maximum.maxFloat(5.1f,2.1f,9.1f);
+        float maximumFloat = maximum.maximumValue(5.1f,2.1f,9.1f);
         Assert.assertEquals(9.1f, maximumFloat,0.0f);
     }
     @Test
     public void givenString_WhenFirstString_IsMaxShouldReturnMax(){
-        String maximumString = maximum.maxString("Pizza","Burger", "Noodles");
+        String maximumString = maximum.maximumValue("Pizza","Burger", "Noodles");
         Assert.assertEquals("Pizza", maximumString);
     }
     @Test
     public void givenString_WhenSecondString_IsMaxShouldReturnMax(){
-        String maximumString = maximum.maxString("Burger","Pizza", "Noodles");
+        String maximumString = maximum.maximumValue("Burger","Pizza", "Noodles");
         Assert.assertEquals("Pizza", maximumString);
     }
     @Test
     public void givenString_WhenThirdString_IsMaxShouldReturnMax(){
-        String maximumString = maximum.maxString("Burger","Noodles", "Pizza");
+        String maximumString = maximum.maximumValue("Burger","Noodles", "Pizza");
         Assert.assertEquals("Pizza", maximumString);
     }
 }
