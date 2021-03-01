c1dc03f9d4507dc4eccb8c7b0f719186e8a6ae99
gautamb97 <www.gautam971997@gmail.com>
2021-03-01 19:37:41 +0530

    Added:Tested first position contains the maximum integer value

--- main/java/com/testmaximum/Maximum.java
+++ main/java/com/testmaximum/Maximum.java
@@ -1,4 +1,18 @@
 package com.testmaximum;
 
 public class Maximum {
+    public static Integer maxInteger(Integer x, Integer y, Integer z){
+        Integer max = x;
+        if(y.compareTo(max) > 0){
+            max = y;
+        }
+        if(z.compareTo(max) > 0){
+            max = z;
+        }
+        printMaxInteger(x, y, z, max);
+        return max;
+    }
+    private static void printMaxInteger(Integer x, Integer y, Integer z, Integer max){
+        System.out.println("Maximum integer value is : "+max);
+    }
 }
--- test/java/com/testmaximum/MaximumTest.java
+++ test/java/com/testmaximum/MaximumTest.java
@@ -1,5 +1,13 @@
 package com.testmaximum;
 
-public class MaximumTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class MaximumTest {
+    @Test
+    public void givenInteger_WhenFirstInteger_IsMaxShouldReturnMax(){
+        Maximum maximum = new Maximum();
+        int maximumInteger = maximum.maxInteger(9,5,2);
+        Assert.assertEquals(9, maximumInteger);
+    }
 }
